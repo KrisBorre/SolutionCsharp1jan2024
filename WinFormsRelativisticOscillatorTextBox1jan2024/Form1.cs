813509b122355da20b56eeb7b0967200187562b2
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-01-02 19:57:59 +0100

    Relativistic oscillator

--- WinFormsRelativisticOscillatorTextBox1jan2024/Form1.cs
+++ WinFormsRelativisticOscillatorTextBox1jan2024/Form1.cs
@@ -26,26 +26,26 @@ namespace WinFormsRelativisticOscillatorTextBox1jan2024
 
             this.ClientSize = new Size(1456, 557);
 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(1352, 13);
+            this.textBox1.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+            this.textBox1.Location = new Point(1352, 13);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(54, 22);
+            this.textBox1.Size = new Size(54, 22);
             this.textBox1.TabIndex = 0;
 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(1113, 12);
+            this.button1.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
+            this.button1.Location = new Point(1113, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new Size(75, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.MouseClick += new MouseEventHandler(this.button1_MouseClick);
 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Anchor = (AnchorStyles.Top | AnchorStyles.Right);
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1232, 15);
+            this.label2.Location = new Point(1232, 15);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.Size = new Size(45, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Ratio of v over c:";
 
