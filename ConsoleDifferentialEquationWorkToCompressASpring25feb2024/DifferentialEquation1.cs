8c0b80eea0e4bf3ac7481974762fb3d690dc5be8
kris_borre <kris_borre@DESKTOP-V0LQE95>
2024-02-26 11:21:35 +0100

    The work required to compress a spring.

---
+++ ConsoleDifferentialEquationWorkToCompressASpring25feb2024/DifferentialEquation1.cs
@@ -0,0 +1,19 @@
+﻿using LibraryDifferentialEquations1jan2024;
+
+namespace ConsoleDifferentialEquationWorkToCompressASpring25feb2024
+{
+    internal class DifferentialEquation1 : DifferentialEquationBaseClass
+    {
+        double k;
+
+        public DifferentialEquation1(double k = 50)
+        {
+            this.k = k;
+        }
+
+        public override double function(double interval, double x, params double[] y)
+        {
+            return k * x;
+        }
+    }
+}
---
+++ ConsoleDifferentialEquationWorkToCompressASpring25feb2024/DifferentialEquationsWorkToCompressASpring.cs
@@ -0,0 +1,13 @@
+﻿using LibraryDifferentialEquations1jan2024;
+
+namespace ConsoleDifferentialEquationWorkToCompressASpring25feb2024
+{
+    internal class DifferentialEquationsWorkToCompressASpring : DifferentialEquationsBaseClass
+    {
+        public DifferentialEquationsWorkToCompressASpring()
+        {
+            this.NumberOfFirstOrderEquations = 1;
+            this[0] = new DifferentialEquation1();            
+        }
+    }
+}
